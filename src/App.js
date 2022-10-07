054dea5b121a96df915923b1132975159bad5945
toru_motomura\tbenc <tbencun156@gmail.com>
2022-10-07 17:19:57 +0900

    Player 完成

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Canvas } from "@react-three/fiber";
 import { Sky } from "@react-three/drei";
 import { Physics } from "@react-three/cannon";
 import { Ground } from "./components/Ground";
+import { Player } from "./components/Player";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Sky sunPosition={[100, 200, 20]} />
         <ambientLight intensity={0.5} />
         <Physics>
+          <Player />
           <Ground />
         </Physics>
       </Canvas>
